4269558c484b82f3dbff9dfede81f4270187e922
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2025-03-10 22:41:26 +0100

    rename ustawienia to settings

---
+++ src/app/components/settings/settings.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SettingsService {
+  constructor(private http: HttpClient) {}
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 400 && error.error.error === 'DuplicateEntry') {
+      return throwError(() => new Error('DuplicateEntry'));
+    }
+    return throwError(() => new Error('Wystąpił błąd serwera.'));
+  }
+
+  getSetNumber(): Observable<any[]> {
+    return this.http
+      .get<any[]>(`${environment.API_URL}/settings/number`)
+      .pipe(catchError(this.handleError));
+  }
+}
