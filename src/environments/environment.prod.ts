b453c25446d6c67f373d33901801f62900baf0a0
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2025-12-22 15:59:48 +0100

    add slash on upload path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   API_URL: 'http://zestawienia.host355495.stronawcal.pl/api',
   FRONT_URL: 'http://zestawienia.host355495.stronawcal.pl',
-  FILES_URL: 'http://zestawienia.host355495.stronawcal.pl/uploads',
+  FILES_URL: 'http://zestawienia.host355495.stronawcal.pl/uploads/',
   FEATURE_FLAG: false,
 };
