d8c4e3bdd25a16f56494da303c8b8eace396f05d
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2026-01-09 12:48:56 +0100

    add new hosting to env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_URL: `${window.location.protocol}//${window.location.host}/api`,
-  FRONT_URL: 'http://zestawienia.host355495.stronawcal.pl',
-  FILES_URL: 'http://zestawienia.host355495.stronawcal.pl/uploads',
+  FRONT_URL: 'http://zestawienia.host119403.stronawcal.pl',
+  FILES_URL: 'http://zestawienia.host119403.stronawcal.pl/uploads',
   FEATURE_FLAG: false,
 };
