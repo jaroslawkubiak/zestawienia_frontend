4a1d4234373b71c6e0518a28617e90cfbd336a8f
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2026-01-18 17:54:04 +0100

    add prod domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_URL: `${window.location.protocol}//${window.location.host}/api`,
-  FRONT_URL: 'http://zestawienia.host119403.stronawcal.pl',
-  FILES_URL: 'http://zestawienia.host119403.stronawcal.pl/uploads',
+  FRONT_URL: 'http://zestawienia.zurawickidesign.pl',
+  FILES_URL: 'http://zestawienia.zurawickidesign.pl/uploads',
   FEATURE_FLAG: false,
 };
