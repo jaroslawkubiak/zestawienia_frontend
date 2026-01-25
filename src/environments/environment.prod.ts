573dcff2a0a10a1ac8d3a255114ecf9f91516d58
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2026-01-25 11:29:47 +0100

    add https to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_URL: `${window.location.protocol}//${window.location.host}/api`,
-  FRONT_URL: 'http://zestawienia.zurawickidesign.pl',
-  FILES_URL: 'http://zestawienia.zurawickidesign.pl/uploads',
+  FRONT_URL: 'https://zestawienia.zurawickidesign.pl',
+  FILES_URL: 'https://zestawienia.zurawickidesign.pl/uploads',
   FEATURE_FLAG: false,
 };
