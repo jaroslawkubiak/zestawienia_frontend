12c1dada504246aba9c299067bc773d4acbf0e03
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2026-02-25 09:47:37 +0100

    add neutral color to settings

--- src/app/components/settings/colors/colors.component.ts
+++ src/app/components/settings/colors/colors.component.ts
@@ -10,24 +10,31 @@ import { Component, OnInit } from '@angular/core';
 export class ColorsComponent implements OnInit {
   accentColors: { color: string; name: string }[] = [];
   burgundColors: { color: string; name: string }[] = [];
+  neutralColors: { color: string; name: string }[] = [];
 
   ngOnInit(): void {
     const root = document.documentElement;
 
     for (let i = 1; i <= 20; i++) {
-      const value = getComputedStyle(root)
+      const valueAccent = getComputedStyle(root)
         .getPropertyValue(`--accent-color-${i.toString().padStart(2, '0')}`)
         .trim();
-      const colorName = i.toString().padStart(2, '0');
-      this.accentColors.push({ color: value, name: colorName });
-    }
+      const colorNameAccent = i.toString().padStart(2, '0');
+      this.accentColors.push({ color: valueAccent, name: colorNameAccent });
 
-    for (let i = 1; i <= 20; i++) {
-      const value = getComputedStyle(root)
+      const valueBurgund = getComputedStyle(root)
         .getPropertyValue(`--burgund-color-${i.toString().padStart(2, '0')}`)
         .trim();
+      const colorNameBurgund = i.toString().padStart(2, '0');
+      this.burgundColors.push({ color: valueBurgund, name: colorNameBurgund });
+    }
+
+    for (let i = 1; i <= 10; i++) {
+      const value = getComputedStyle(root)
+        .getPropertyValue(`--neutral-color-${i.toString().padStart(2, '0')}`)
+        .trim();
       const colorName = i.toString().padStart(2, '0');
-      this.burgundColors.push({ color: value, name: colorName });
+      this.neutralColors.push({ color: value, name: colorName });
     }
   }
 }
