24f55fe10d528b34b9046f24c327890b4c42d88f
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2026-03-22 23:21:07 +0100

    port change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:3005/api',
+  API_URL: 'http://localhost:3000/api',
   FRONT_URL: 'http://localhost:4200',
-  FILES_URL: 'http://localhost:3005/uploads',
+  FILES_URL: 'http://localhost:3000/uploads',
   FEATURE_FLAG: true,
 };
